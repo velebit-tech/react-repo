c4a4feec6d65c3246d2059144a88bb2894bf760f
Mario Markovic <velebit.business@gmail.com>
2020-03-03 22:57:23 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ class App extends Component {
     ]
   }
 
+  changeAnimalHandler = () => {
+    console.log('animal changed lol');
+  }
+
   render() {
     return (
     <div className="App">
       <h2>Hello react.</h2>
+      <button onClick={this.changeAnimalHandler}>Change Animal</button>
       <Animal type={this.state.animals[0].type} skin={this.state.animals[0].skin} />
       <Animal type={this.state.animals[1].type} skin={this.state.animals[1].skin}>Meow.</Animal>
     </div>
