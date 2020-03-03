9b9046f77dcdd2f56dcdd22f0f3942da9dae8657
Mario Markovic <velebit.business@gmail.com>
2020-03-03 20:46:58 +0100

    added first component

---
+++ src/Animal/Animal.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const animal = () => {
+  return <div>I am a cat!</div>
+}
+
+export default animal;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Animal/Animal.js'
 
 function App() {
   return (
     <div className="App">
       <h2>Hello react.</h2>
+      <Person />
+      <Person />
     </div>
   );
 }
