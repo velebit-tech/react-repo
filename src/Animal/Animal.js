98ea1fae95e4cd62e3bce0994a138904d15d4082
Mario Markovic <velebit.business@gmail.com>
2020-03-16 16:36:14 +0100

    empty

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -5,8 +5,13 @@ import './Animal.css';
 const animal = (props) => {
   return (
     <div className="Animal">
-      <p onClick={props.clicked}>I am a {props.type} and I have {props.skin} skin.</p>
-      <input type="text" onChange={props.skinchange} value={props.skin} />
+      <p onClick={props.clicked}>
+        I am a {props.type} and I have {props.skin} skin.
+      </p>
+      <input 
+        type="text" 
+        onChange={props.skinchange} 
+        value={props.skin} />
     </div>
   )
 };
