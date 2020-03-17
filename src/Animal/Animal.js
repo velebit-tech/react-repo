df13bd3c252758d3777080eda047964aa84ec289
Mario Markovic <velebit.business@gmail.com>
2020-03-17 13:06:04 +0100

    added CSS module styling

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import './Animal.css';
+import classes from './Animal.module.css';
 
 const animal = (props) => {
   return (
-    <div className="Animal">
+    <div className={classes.Animal}>
       <p onClick={props.clicked}>
         I am a {props.type} and I have {props.skin} skin.
       </p>
